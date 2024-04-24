f0aff85764bf74dff2d8e0bdc47855740d7c578f
oscarraichert <oscarraichert@hotmail.com>
2024-04-23 22:35:00 -0300

    implementando form

--- lib/src/extract/new_extract.dart
+++ lib/src/extract/new_extract.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class NewExtractWidget extends StatelessWidget {
+class NewExtractWidget extends StatefulWidget {
   const NewExtractWidget({super.key});
+
+  @override
+  State<NewExtractWidget> createState() => _NewExtractState();
+}
+
+class _NewExtractState extends State<NewExtractWidget> {
+  final descriptionController = TextEditingController();
+  final valueController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -12,14 +20,62 @@ class NewExtractWidget extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(10),
         child: Container(
-          decoration: BoxDecoration(
-              border: Border.all(color: Theme.of(context).dividerColor),
-              color: Theme.of(context).hoverColor,
-              borderRadius: const BorderRadius.all(Radius.circular(20))),
+          decoration: BoxDecoration(border: Border.all(color: Theme.of(context).dividerColor), borderRadius: const BorderRadius.all(Radius.circular(20))),
           alignment: Alignment.center,
-          child: const Column(
+          child: Column(
             children: [
-              Text('Form', textAlign: TextAlign.center),
+              Form(
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text('Description:', style: TextStyle(fontWeight: FontWeight.bold)),
+                      ),
+                      TextFormField(
+                        controller: descriptionController,
+                        maxLength: 35,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+                          isDense: true,
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Enter a description';
+                          }
+                          return null;
+                        },
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text('Value:', style: TextStyle(fontWeight: FontWeight.bold)),
+                      ),
+                      TextFormField(
+                        controller: valueController,
+                        maxLength: 35,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+                          isDense: true,
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Enter a value';
+                          }
+                          return null;
+                        },
+                      ),
+                      ElevatedButton(
+                        child: const Text('Submit'),
+                        onPressed: () => {
+                          print(descriptionController.text),
+                          print(valueController.text)
+                        },
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
